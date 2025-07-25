d4cbd0219642726191211560a528e2f26ee74a36
Gaukhar <sagit.gaukhar.775@gmail.com>
2025-07-25 12:15:11 +0500

    Update: add custom separator print task
    Replaced basic input-output task with a version that prints three inputs using a custom separator.

---
+++ 01-input-output.py
@@ -0,0 +1,14 @@
+# Task: Input and Output
+# Description: Print three values with a custom separator from input
+
+# Read a custom separator and three values
+sep1 = input()
+a = input()
+b = input()
+c = input()
+
+# Print them using the custom separator
+print(a, b, c, sep=sep1)
+
+# Stepik Course: Programming in Python
+# Topic: Input and Output
